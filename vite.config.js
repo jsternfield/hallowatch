a6bf1b4eae1e29a43fb4d0988d5f5aa8e0344b19
jsternfield <jay.sternfield@gmail.com>
2026-03-06 16:12:55 -0500

    Fixed relative path issue with js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 
+// When deploying to GitHub Pages under a repo such as
+// https://<username>.github.io/hallowatch/ we need to set
+// a base so that all asset URLs are generated relative to that
+// path. The HTML can also use the special `%BASE_URL%` placeholder
+// which will be replaced at build time.
 export default defineConfig({
+  base: '/hallowatch/',
   server: {
     port: 5173,
     strictPort: false
